9c10de04dda9948d7186fe126b0fa1df0c691216
tumaa <ramanfatuu@gmail.com>
2019-08-18 21:04:59 +0300

    added a condition

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 function GetValue(){
+        var male= document.getElementById('male').value);
+        var female= document.getElementById('female').value);
+
+
     var myarray= new Array("Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama");
     var random = myarray[Math.floor(Math.random() * myarray.length)];
    document.getElementById("message").innerHTML=random;
