6ccf182c8bd8ccda235a6c14cbd05b59fef1f747
tumaa <ramanfatuu@gmail.com>
2019-08-18 22:32:49 +0300

    declared random names in an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function GetValue(female,male)
 {
-    var myarray= new Array("item1","item2","item3");
+    var myarray= new Array("Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame","Akosua","Adwoa","Abenaa","Akua","Yaa", "Afua","Ama");
     var random = myarray[Math.floor(Math.random() * myarray.length)];
    document.getElementById("message").innerHTML=random;
 }
