cba901550b75b793324b9e24eb096efd12a0af8a
tumaa <ramanfatuu@gmail.com>
2019-08-24 11:15:35 +0300

    trying to get the month and converting them to single day

--- scripts.js
+++ scripts.js
@@ -57,4 +57,110 @@ function validation() {
         alert("invalid month");
        return false;
     }
-   
+     if(month===1 || month>2){
+  if (date>ListofDays[month-1])
+  {
+  alert('Invalid date format!');
+  return false;
+  }
+  }
+  if(month===2){
+  var lyear = false;
+  if ( (!(year % 4) && year % 100) || !(year % 400)) 
+  {
+      
+  lyear = true;
+  }
+  if ((lyear==false) && (date>=29))
+  {
+  alert('Invalid date format!');
+  return false;
+  }
+  if ((lyear==true) && (date>29))
+  {
+  alert('Invalid date format!');
+  return false;
+  }
+  }
+  if(isNaN(year) || year === "" ||year === null || year <1900 || year > currentYear){
+    document.getElementById('three').innerHTML = "invalid year";
+    document.getElementById('three').style.color = "red"
+        alert("invalid year");
+        return false;
+ }
+    if (gen[0].checked === false && gen[1].checked === false) {
+        document.getElementById('four').innerHTML = "select gender";
+        document.getElementById('four').style.color = "red"
+        alert('select gender');
+        
+     } else {
+         for (i = 0; i < gen.length; i++) {
+             if (gen[i].checked){
+                alert(gen[i].value);
+                    var gend = gen[i].value;
+         }
+                    //return gend;
+                    var userInputs ={
+                        userDate: date,
+                        userMonth: month,
+                        userYear: year,
+                        userGender: gend
+                    };
+                    
+                    return userInputs;
+                    }
+                    
+                
+                }           
+
+}
+
+ function execute(){
+     var a = validation();
+     var DD = parseInt(a.userDate);
+     var MM = parseInt(a.userMonth);
+     var newYear = a.userYear;
+     var YY = parseInt(newYear.slice(2,4));
+     var CC = parseInt(newYear.slice(0,2));
+     var newGender = a.userGender;
+
+var a = Math.floor((14 - MM) / 12);
+var y = newYear - a;
+ var m = MM + 12 * a - 2;
+ var dayOfTheWeek = (DD + y + Math.floor(y / 4) - Math.floor(y / 100) +
+ Math.floor(newYear / 400) + Math.floor((31 * m) / 12)) % 7;
+ 
+ var daysOfaweek = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday',       
+         'Thursday', 'Friday', 'Saturday');
+ 
+
+var realDay = daysOfaweek[dayOfTheWeek];
+
+
+if (newGender ==="male") {
+for (var name in maleNames) {
+        if (maleNames.hasOwnProperty(name)) {
+            if (name === realDay) {
+                document.getElementById('ans').innerHTML = maleNames[name];
+                document.getElementById('three').style.backgroundColor = "black";
+                document.getElementById('three').style.color = "white";
+                alert("Your Akan name is "+maleNames[name]);
+            }
+        }
+    }
+
+ }else {
+    for (var name in femaleNames) {
+            if (femaleNames.hasOwnProperty(name)) {
+                if (name === realDay) {
+                    document.getElementById('ans').innerHTML = femaleNames[name];
+                    document.getElementById('three').style.backgroundColor = "black";
+                document.getElementById('three').style.color = "white";
+                    alert("Your Akan name is "+femaleNames[name]);
+                }
+            }
+        }
+    
+     }
+}
+
